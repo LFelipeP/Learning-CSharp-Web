114f067a5fab13cf3125beb5518f2cc13b5500e3
LFelipeP <luizfelipepina@gmail.com>
2021-05-19 22:33:05 -0300

    Confis of Native Injections

---
+++ Template.Application/Services/UserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Template.Application.Interfaces;
+
+namespace Template.Application.Services
+{
+    public class UserService : IUserService
+    {
+        public void Test() 
+        { 
+
+        }
+
+    }
+}
---
+++ Template.IoC/NativeInjector.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using Template.Application.Services;
+using Template.Application.Interfaces;
+
+namespace Template.IoC
+{
+    public static class NativeInjector
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+
+            services.AddScoped<IUserService, UserService>();
+
+        }
+    }
+}
